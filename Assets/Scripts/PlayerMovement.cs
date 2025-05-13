c4ba93660354d5ce488e3808613919d574d16079
Nathaniel Kong <nathanielkong1@gmail.com>
2025-05-13 19:59:03 +1000

    player movement and ground

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveX, 0, moveZ);
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
+}
